9d7de57d19074b8483f057c029829062d1be539e
Nat Burgwyn <burgwyn@gmail.com>
2017-11-27 15:15:52 -0800

    Creating production build

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ var config = {
     },
     output: {
         filename: '[name].js',
-        path: path.join(__dirname, '.'),
+        path: path.join(__dirname, 'build'),
         sourceMapFilename: '[file].map',
         publicPath: '/'
     },
