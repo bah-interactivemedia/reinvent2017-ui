fc75542bf37d2616818baebbfad8e0f8d1290e5b
Michael Bray <bray_michael@bah.com>
2017-11-27 20:41:43 -0800

    Responsive fitness tests

--- src/js/components/FitnessTests.jsx
+++ src/js/components/FitnessTests.jsx
@@ -15,19 +15,19 @@ export default class FitnessTests extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-4">
+                <div className="col-12 col-md-4">
                     <Link className="track-link" to="/bodyComposition">
                         <span className="oi oi-person" title="person" aria-hidden="true"></span>
                         <p>Body Composition</p>
                     </Link>
                 </div>
-                <div className="col-4">
+                <div className="col-12 col-md-4">
                     <Link className="track-link" to="/mileRun">
                         <span className="oi oi-media-skip-forward" title="media-skip-forward" aria-hidden="true"></span>
                         <p>Mile Run</p>
                     </Link>
                 </div>
-                <div className="col-4">
+                <div className="col-12 col-md-4">
                     <Link className="track-link" to="/curlUps">
                         <span className="oi oi-data-transfer-upload" title="data-transfer-upload" aria-hidden="true"></span>
                         <p>Curl Ups</p>
