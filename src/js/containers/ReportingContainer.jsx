81c97212047c1e7683c17c48c2150bae6d3dd3e9
Michael Bray <bray_michael@bah.com>
2017-11-27 20:44:00 -0800

    More responsiveness

--- src/js/containers/ReportingContainer.jsx
+++ src/js/containers/ReportingContainer.jsx
@@ -31,24 +31,24 @@ export class ReportingContainer extends React.Component {
     return (
         <div className="container-fluid">
             <div className="row justify-content-md-center">
-                <div className="col col-lg-4">
+                <div className="col col-12 col-lg-4">
                     <ReportingItem title="Average BMI">
                         <ReportingValue value={this.props.reportList.bmiAvg} />
                     </ReportingItem>
                 </div>
-                <div className="col col-lg-4">
+                <div className="col col-12 col-lg-4">
                     <ReportingItem title="Average Body Fat %">
                         <ReportingValue value={`${this.props.reportList.bodyfatCalcAvg} %`} />
                     </ReportingItem>
                 </div>
             </div>
             <div className="row justify-content-md-center">
-                <div className="col col-lg-4">
+                <div className="col col-12 col-lg-4">
                     <ReportingItem title="Mile Run">
                         <ReportingDonut data={[{angle:25},{angle:11},{angle:33}]} />
                     </ReportingItem>
                 </div>
-                <div className="col col-lg-4">
+                <div className="col col-12 col-lg-4">
                     <ReportingItem title="Curl Ups">
                         <ReportingDonut data={[{angle:20},{angle:7},{angle:42}]} />
                     </ReportingItem>
