f706741aecb2d262c8c97d4ea83d4562aebe6f68
Nat Burgwyn <burgwyn@gmail.com>
2017-11-27 18:57:00 -0800

    Redux for reports

--- src/js/containers/ReportingContainer.jsx
+++ src/js/containers/ReportingContainer.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as reportsActions from '../actions/reportsAction';
 
 import ReportingItem from '../components/ReportingItem';
 import ReportingValue from '../components/ReportingValue';
 import ReportingDonut from '../components/ReportingDonut';
 
-const ReportingContainer = () => {
+const propTypes = {
+    reportsFetchData: PropTypes.func.isRequired,
+    reportList: PropTypes.object
+};
+
+const defaultProps = {
+    reportList: {}
+};
+
+export class ReportingContainer extends React.Component {
+
+    componentWillMount() {
+        this.getData();
+    }
+
+    getData() {
+        this.props.reportsFetchData();
+    }
+    render() {
     return (
         <div className="container-fluid">
             <div className="row justify-content-md-center">
@@ -33,6 +56,15 @@ const ReportingContainer = () => {
             </div>
         </div>
     );
+    }
 };
 
-export default ReportingContainer;
+ReportingContainer.propTypes = propTypes;
+ReportingContainer.defaultProps = defaultProps;
+
+export default connect(
+ (state) => ({
+    reportList: state.reports.reportList,
+ }),
+ (dispatch) => bindActionCreators(reportsActions, dispatch)
+ )(ReportingContainer); 
